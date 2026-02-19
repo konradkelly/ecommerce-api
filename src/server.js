d615f374cc1bcbae3d2077b0b5a10c3d9bb3789b
samkosal <67755908+samkosal@users.noreply.github.com>
2026-02-18 17:38:54 -0800

    adsfadsf

--- src/server.js
+++ src/server.js
@@ -1,10 +1,17 @@
 import app from './app.js';
 import dotenv from 'dotenv';
+import mysql from 'mysql2/promise.js';
 
 //read environment variables
 dotenv.config();
 
-const port = process.env.PORT;
-app.listen(port, () => {
-    console.log(`Server started on http://localhost:${port}`);
+//destructure our env values
+const { PORT, DB_DATABASE, DB_HOST, DB_PORT,
+        DB_USER, DB_PASSWORD } = process.env;
+
+
+// const port = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`Server started on http://localhost:${PORT}`);
 })
