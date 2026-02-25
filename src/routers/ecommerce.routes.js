0167b7f39ccaf0129947c2e00a82d84bec3147af
samkosal <67755908+samkosal@users.noreply.github.com>
2026-02-25 13:17:14 -0800

    updated routing comment

--- src/routers/ecommerce.routes.js
+++ src/routers/ecommerce.routes.js
@@ -4,15 +4,15 @@ import * as ecomCtl from '../controllers/ecommerce.controller.js';
 
 const router = Router();
 
-// http://localhost:8001/
+// http://localhost:8001/api
 router.get("/", ecomCtl.landingPage);
-// http://localhost:8001/data
+// http://localhost:8001/api/data
 router.get('/data', ecomCtl.getData);
-// http://localhost:8001/login
+// http://localhost:8001/api/login
 router.get('/login', ecomCtl.login);
-// http://localhost:8001/register
+// http://localhost:8001/api/register
 router.get('/register', ecomCtl.register);
-// http://localhost:8001/products
+// http://localhost:8001/api/products
 router.get('/products', ecomCtl.products);
 // router.get('/products/:id', ecomCtl.products);
 
